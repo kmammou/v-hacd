41f38878e47005876b52ecdb31593db563cc3834
Khaled Mammou <kmamou@gmail.com>
2015-01-12 05:07:41 -0500

    include opencl kernels in executable (to be improved later)

--- src/VHACD_Lib/cl/vhacdKernels.cl
+++ src/VHACD_Lib/cl/vhacdKernels.cl
@@ -1,23 +1,30 @@
 __kernel void ComputePartialVolumes(__global short4 * voxels,
-                                      const  int      numVoxels,
-                                      const  float4   plane,
-                                      const  float4   minBB,
+                                    const    int      numVoxels,
+                                    const    float4   plane,
+                                    const    float4   minBB,
                                     __local  uint4 *  localPartialVolumes,
                                     __global uint4 *  partialVolumes)
 {
-    int localId   = get_local_id(0);
+    int localId = get_local_id(0);
     int groupSize = get_local_size(0);
     int i0 = get_global_id(0) << 2;
-    uint  pVol[4] = { 0, 0, 0, 0 };
+    uint  pVol[4];
     short4 voxel;
     float4 pt;
     float  d;
-    for (int i = 0; (i0+i < numVoxels) && (i < 4); ++i)
+    for (int i = 0; i < 4; ++i)
     {
-        voxel   = voxels[i+i0];
-        pt      = (float4)(voxel.s0 * minBB.s3 + minBB.s0, voxel.s1 * minBB.s3 + minBB.s1, voxel.s2 * minBB.s3 + minBB.s2, 1.0f);
-        d       = dot(plane, pt);
-        pVol[i] = d >= 0.0f;
+        if (i0 + i < numVoxels)
+        {
+            voxel = voxels[i + i0];
+            pt = (float4)(voxel.s0 * minBB.s3 + minBB.s0, voxel.s1 * minBB.s3 + minBB.s1, voxel.s2 * minBB.s3 + minBB.s2, 1.0f);
+            d = dot(plane, pt);
+            pVol[i] = d >= 0.0f;
+        }
+        else
+        {
+            pVol[i] = 0.0f;
+        }
     }
     localPartialVolumes[localId] = *((uint4 *)(pVol));
     barrier(CLK_LOCAL_MEM_FENCE);
@@ -35,7 +42,6 @@ __kernel void ComputePartialVolumes(__global short4 * voxels,
         partialVolumes[get_group_id(0)] = localPartialVolumes[0];
     }
 }
-
 __kernel void ComputePartialSums(__global uint4 * data,
                                    const  int     dataSize,
                                  __local  uint4 * partialSums) 
--- src/VHACD_Lib/src/VHACD.cpp
+++ src/VHACD_Lib/src/VHACD.cpp
@@ -38,6 +38,84 @@
 #define ABS(a) (((a)<0) ? -(a) : (a))
 #define ZSGN(a) (((a)<0) ? -1 : (a)>0 ? 1 : 0)
 
+//#define OCL_SOURCE_FROM_FILE
+
+#ifndef OCL_SOURCE_FROM_FILE
+        const char * oclProgramSource = "\
+__kernel void ComputePartialVolumes(__global short4 * voxels,\
+                                    const    int      numVoxels,\
+                                    const    float4   plane,\
+                                    const    float4   minBB,\
+                                    __local  uint4 *  localPartialVolumes,\
+                                    __global uint4 *  partialVolumes)\
+        {\
+            int localId = get_local_id(0);\
+            int groupSize = get_local_size(0);\
+            int i0 = get_global_id(0) << 2;\
+            uint  pVol[4];\
+            short4 voxel;\
+            float4 pt;\
+            float  d;\
+            for (int i = 0; i < 4; ++i)\
+            {\
+                if (i0 + i < numVoxels)\
+                {\
+                    voxel = voxels[i + i0];\
+                    pt = (float4)(voxel.s0 * minBB.s3 + minBB.s0, voxel.s1 * minBB.s3 + minBB.s1, voxel.s2 * minBB.s3 + minBB.s2, 1.0f);\
+                    d = dot(plane, pt); \
+                    pVol[i] = d >= 0.0f;\
+                }\
+                else\
+                {\
+                    pVol[i] = 0;\
+                }\
+            }\
+            localPartialVolumes[localId] = *((uint4 *)(pVol));\
+            barrier(CLK_LOCAL_MEM_FENCE);\
+            for (int i = groupSize >> 1; i > 0; i >>= 1)\
+            {\
+                if (localId < i)\
+                {\
+                    localPartialVolumes[localId] += localPartialVolumes[localId + i];\
+                }\
+                barrier(CLK_LOCAL_MEM_FENCE);\
+            }\
+            if (localId == 0)\
+            {\
+                partialVolumes[get_group_id(0)] = localPartialVolumes[0];\
+            }\
+        }\
+        __kernel void ComputePartialSums(__global uint4 * data,\
+                                           const  int     dataSize,\
+                                           __local  uint4 * partialSums)\
+        {\
+            int globalId = get_global_id(0);\
+            int localId = get_local_id(0);\
+            int groupSize = get_local_size(0);\
+            if (globalId < dataSize)\
+            {\
+                partialSums[localId] = data[globalId];\
+            }\
+            else\
+            {\
+                partialSums[localId] = (0, 0, 0, 0);\
+            }\
+            barrier(CLK_LOCAL_MEM_FENCE);\
+            for (int i = groupSize >> 1; i > 0; i >>= 1)\
+            {\
+                if (localId < i)\
+                {\
+                    partialSums[localId] += partialSums[localId + i];\
+                }\
+                barrier(CLK_LOCAL_MEM_FENCE);\
+            }\
+            if (localId == 0)\
+            {\
+                data[get_group_id(0)] = partialSums[0];\
+            }\
+        }";
+#endif //OCL_SOURCE_FROM_FILE
+
 namespace VHACD
 {
     IVHACD * CreateVHACD(void)
@@ -48,6 +126,10 @@ namespace VHACD
                         IUserLogger * const logger)
     {
 #ifdef CL_VERSION_1_1
+
+
+
+
         m_oclDevice  = (cl_device_id *) oclDevice;
         cl_int error;
         m_oclContext = clCreateContext(NULL, 1, m_oclDevice, NULL, NULL, &error);
@@ -60,6 +142,7 @@ namespace VHACD
             return false;
         }
 
+#ifdef OCL_SOURCE_FROM_FILE
         std::string cl_files = OPENCL_CL_FILES;
         // read kernal from file
 #ifdef _WIN32
@@ -75,9 +158,12 @@ namespace VHACD
         fread(program_buffer, sizeof(char), program_size, program_handle);
         fclose(program_handle);
         // create program
-
         m_oclProgram = clCreateProgramWithSource(m_oclContext, 1, (const char**)&program_buffer, &program_size, &error);
         delete[] program_buffer;
+#else
+        size_t program_size = strlen(oclProgramSource);
+        m_oclProgram = clCreateProgramWithSource(m_oclContext, 1, (const char**)&oclProgramSource, &program_size, &error);
+#endif
         if (error != CL_SUCCESS)
         {
             if (logger)
