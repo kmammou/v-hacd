ca76d54b0cd374315506766858b06a6bea256e05
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-11-10 20:46:41 +0100

    Fix build when linking against musl
    musl doesn't define `PTHREAD_MUTEX_RECURSIVE`, so it has to be done
    manually.

--- src/VHACD_Lib/inc/vhacdMutex.h
+++ src/VHACD_Lib/inc/vhacdMutex.h
@@ -71,7 +71,8 @@
 #include <pthread.h>
 #endif
 
-#if defined(__APPLE__)
+// PTHREAD_MUTEX_RECURSIVE_NP isn't available in musl
+#if defined(__APPLE__) || !defined(__GLIBC__)
 #define PTHREAD_MUTEX_RECURSIVE_NP PTHREAD_MUTEX_RECURSIVE
 #endif
 
